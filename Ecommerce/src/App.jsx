24ed182ccf90890fa05bf6933b075d1d75ff70af
Nahuel Nuñez <nunezspinelli8@gmail.com>
2023-09-17 12:31:25 -0300

    Eliminacion de Contador

--- Ecommerce/src/App.jsx
+++ Ecommerce/src/App.jsx
@@ -1,19 +1,18 @@
-import { useState } from 'react'
+
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-
       <div className='logo'>
         <h2>Proximamente</h2>
         <h1>Luna Mora</h1>
         <p>Lenceria de Autor</p>
       </div>
+
       <div className="card">
 
       </div>
